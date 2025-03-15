bdc584cbffd6f5fd7549718fa6fb579842fa0a60
Sabi <63137224+ijf03@users.noreply.github.com>
2025-03-15 14:01:03 +1100

    Added tailwindcss and Tasklist form

---
+++ src/components/TasklistForm.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+
+interface AddTaskFormProps {
+    onSubmit: (title: string) => void
+}
+
+export default function AddTaskForm({onSubmit}: AddTaskFormProps) {
+
+    const [input, setInput] = useState("")
+
+        function handleSubmit(e:React.FormEvent<HTMLFormElement>) {
+            e.preventDefault()
+            if(!input.trim()) 
+                return;
+            onSubmit(input);
+            setInput("");
+            
+        }
+    return (
+        <div>
+            <form className="flex" onSubmit={handleSubmit}>
+                <input 
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                        placeholder="What shit do you need to do?" 
+                        className="ounded-s-md grow border border-gray-400"
+                />
+                <button 
+                type="submit"
+                className="bg-emerald-500 text-white rounded-e-md hover:bg-emerald-800"
+                >
+                    Add
+                </button>
+            </form>
+        </div>
+    )
+}
